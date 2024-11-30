34ef8928c0351c09fd6509b7eebd48fd555792db
nickolayArbuzov <nickarbuzov@yandex.by>
2024-11-30 18:55:49 +0300

    feat: add tasks

---
+++ lesson_5/tasks.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+
+# 1:
+def nn(input, weights):
+    return input.dot(weights)
+
+
+def get_error(true_prediction, prediction):
+    return (true_prediction - prediction) ** 2
+
+
+input = np.array(
+    [
+        [150, 40],
+        [170, 80],
+        [160, 90],
+        [180, 90],
+        [160, 45],
+        [170, 57],
+        [175, 75],
+        [165, 50],
+    ]
+)
+weigths = np.array([0.2, 0.3])
+
+true_predictions = np.array([0, 100, 100, 100, 0, 0, 100, 0])
+learning_rate = 0.000061
+
+for i in range(1):
+    error = 0
+    delta = 0
+    for j in range(len(input)):
+        prediction = nn(input[j], weigths)
+        error += get_error(true_predictions[j], prediction)
+        print(
+            "Prediction: %s, True_prediction: %s, Weights: %s"
+            % (prediction, true_predictions[j], weigths)
+        )
+        delta += (prediction - true_predictions[j]) * input[j] * learning_rate
+    weigths -= delta / len(input)
+    print("Error: %.10f", (error))
+    print("-------------------------")
+
+# 2:
+input = np.array([[20, 1], [10, -9], [-2, -8]])
+weigths = np.array([0.1, 0.1])
+true_predictions = np.array([21, 1, -10])
+learning_rate = 0.001
+
+for i in range(50):
+    error = 0
+    for j in range(len(input)):
+        prediction = nn(input[j], weigths)
+        error += get_error(true_predictions[j], prediction)
+        print(
+            "Prediction: %s, True_prediction: %s, Weights: %s"
+            % (prediction, true_predictions[j], weigths)
+        )
+        delta = (prediction - true_predictions[j]) * input[j] * learning_rate
+        weigths -= delta
+    print("Error: %.10f", (error))
+    print("-------------------------")
+
+print(nn(np.array([[12, 4]]), weigths))
+print(nn(np.array([[3, -8]]), weigths))
